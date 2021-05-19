d98aa5cd9114e7024de42fc7e92e6fc1533e3798
Lena <84328178+Lena95@users.noreply.github.com>
2021-05-19 11:40:29 -0300

    Update olamundo.py
    Coloquei a mensagem07

--- olamundo.py
+++ olamundo.py
@@ -2,6 +2,7 @@ mensagem = 'Olá universo!!!'
 mensagem = 'Bom dia! Tudo bem com você?'
 mensagem2 = 'Olá mundo'
 mensagem001 = 'Bem-Vindos ao arquivo da 2020115'
+mensagem07 = 'Bem vindo ao curso de desenvolvimento de sistemas!'
 
 print(mensagem)
 
